a4ed944aaacc23bc69a1827e85a820bfbfbe10ef
Ally Smith <allysmith@Allys-MacBook-Air.local>
2018-03-12 10:15:43 -0500

    updated db schema and .gitignore

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,14 @@ CREATE DATABASE buyersWorkbench_db;
 
 USE buyersWorkbench_db;
 
-CREATE TABLE parts
-(
-pn INT IDENTITY(17922,1) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE parts (
+
+pn INT AUTO_INCREMENT PRIMARY KEY,
 description VARCHAR(100) NOT NULL,
 uom VARCHAR(2) NOT NULL,
-buyer INT(4) NOT NULL
-vendor VARCHAR(100) NULL,
-vendor_pn VARCHAR(100) NULL,
+buyer INT(4) NOT NULL,
+vendor VARCHAR(100) NOT NULL,
+vendor_pn VARCHAR(100) NOT NULL,
 lt_days INT NOT NULL,
 cost DECIMAL(10,2) NOT NULL,
 sales_price DECIMAL(10,2) NOT NULL,
@@ -23,25 +23,28 @@ qoh INT NULL,
 ss INT NULL,
 committed INT NULL,
 active BOOLEAN NOT NULL DEFAULT 0
+
 );
+
+ALTER TABLE parts AUTO_INCREMENT=17922;
 
 CREATE TABLE purchase_orders
 (
 po_num INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-vendor VARCHAR(100) NOT NULL,
+vendor VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE purchase_order_lines
 (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-po_number INT NOT NULL,
+po_num INT NOT NULL,
 po_ln INT NOT NULL,
 pn INT NOT NULL,
 order_qty INT NOT NULL,
 delivered_qty INT NOT NULL,
 due_date DATE NOT NULL,
 open BOOLEAN NOT NULL DEFAULT 0,
-FOREIGN KEY (po_number) REFERENCES purchase_orders(po_number)
+FOREIGN KEY (po_num) REFERENCES purchase_orders (po_num)
 );
 
 
