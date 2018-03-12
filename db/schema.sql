8440fec2a6c5ebe75ebe413068d3976fa4d34e79
Elliot Reymond-Christensen <elliotchristensen@gmail.com>
2018-03-12 10:15:54 -0500

    added DS Store to git ignore

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ USE buyersWorkbench_db;
 
 CREATE TABLE parts
 (
-pn INT IDENTITY(17922,1) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+pn INT IDENTITY(17922,1) PRIMARY KEY,
 description VARCHAR(100) NOT NULL,
 uom VARCHAR(2) NOT NULL,
 buyer INT(4) NOT NULL
@@ -24,6 +24,10 @@ ss INT NULL,
 committed INT NULL,
 active BOOLEAN NOT NULL DEFAULT 0
 );
+
+INSERT INTO parts (description, uom, buyer, vendor, vendor_pn, lt_days, cost, sales_price, ord_qty, quantity, qoh, ss, committed)
+
+VALUES ("Mac Book - 15in", "EA", "2150", "APPLE", "HXY567229", "20", "1500", "2000", "1", "10", "3", "2", "1"), ("Mac Book - 11in", "EA", "2150", "APPLE", "FHG6261876", "15", "1200", "1800", "1", "9", "2", "2", "1"), ("Mircosoft laptop - 11in", "EA", "2050", "DELL", "67239202020", "15", "1200", "1800", "1", "9", "2", "2", "1") ;
 
 CREATE TABLE purchase_orders
 (
