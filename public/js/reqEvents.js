5aa83a33b6858416f04e686dc425778265d46975
Elliot Reymond-Christensen <elliotchristensen@gmail.com>
2018-03-12 14:02:37 -0500

    Added Req event listener

---
+++ public/js/reqEvents.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  var reqArray= [];
+  $('#submitBtn').on("click", function(event){
+    event.preventDefault();
+    var lineCount= 1;
+    $('tbody').find('tr').each(function(){
+      if ($(this).find('.checkbox').is(':checked')) {
+        var poLine= {
+          "vendor": $(this).find('.vendorInput').val(),
+          "po_ln": lineCount,
+          "pn": $(this).find('.pn').text(),
+          "qty": $(this).find('.qtyInput').val(),
+          "date": $(this).find('.dateInput').val(),
+        };
+        lineCount++;
+        reqArray.push(poLine);
+      }
+      $.ajax("/api/purchase", {
+        type: "POST",
+        data: reqArray
+      }).then(function(){
+        location.reload();
+      });
+    });
+    console.log(reqArray);
+  });
+});
