faf71eaddd64ed3a847ef3c276f494b23b70c99c
Elliot Reymond-Christensen <elliotchristensen@gmail.com>
2018-03-12 09:45:02 -0500

    schema update NOT nulls

--- db/schema.sql
+++ db/schema.sql
@@ -9,14 +9,14 @@ USE buyersWorkbench_db;
 CREATE TABLE parts
 (
 pn INT IDENTITY(17922,1) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-description VARCHAR(100) NULL,
-uom VARCHAR(2) NULL,
-buyer INT(4) NULL
+description VARCHAR(100) NOT NULL,
+uom VARCHAR(2) NOT NULL,
+buyer INT(4) NOT NULL
 vendor VARCHAR(100) NULL,
 vendor_pn VARCHAR(100) NULL,
-lt_days INT NULL,
-cost DECIMAL(10,2) NULL,
-sales_price DECIMAL(10,2) NULL,
+lt_days INT NOT NULL,
+cost DECIMAL(10,2) NOT NULL,
+sales_price DECIMAL(10,2) NOT NULL,
 ord_qty INT NULL,
 quantity INT NULL,
 qoh INT NULL,
@@ -27,16 +27,23 @@ active BOOLEAN NOT NULL DEFAULT 0
 
 CREATE TABLE purchase_orders
 (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-po_num INT NULL,
-po_ln INT NULL,
-pn INT NULL,
-vendor VARCHAR(100) NULL,
-order_qty INT NULL,
-delivered_qty INT NULL,
-due_date DATE NULL,
-open BOOLEAN NOT NULL DEFAULT 0
+po_num INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+vendor VARCHAR(100) NOT NULL,
 );
+
+CREATE TABLE purchase_order_lines
+(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+po_number INT NOT NULL,
+po_ln INT NOT NULL,
+pn INT NOT NULL,
+order_qty INT NOT NULL,
+delivered_qty INT NOT NULL,
+due_date DATE NOT NULL,
+open BOOLEAN NOT NULL DEFAULT 0,
+FOREIGN KEY (po_number) REFERENCES purchase_orders(po_number)
+);
+
 
 CREATE TABLE sales_orders
 (
